5bbbcc646c5eb8c39f179ad91877b1ef137cc115
Kim Robert <kimrobert.nb@gmail.com>
2024-02-02 10:54:20 +0100

    t001 feat: add http client

--- MtgClient/MtgClient.cs
+++ MtgClient/MtgClient.cs
@@ -1,13 +1,30 @@
-﻿namespace MtgClient;
+﻿using System.Net.Http.Headers;
 
-public static class MtgClient
+namespace MtgClient;
+
+public class Client
 {
-    public static bool StartsWithUpper(this string? str)
-    {
-        if (string.IsNullOrWhiteSpace(str))
-            return false;
+    public HttpClient httpClient;
+    public string Api;
 
-        char ch = str[0];
-        return char.IsUpper(ch);
+    public Client()
+    {
+        this.httpClient = new HttpClient();
+        this.Api = "https://api.scryfall.com";
+        httpClient.DefaultRequestHeaders.Accept.Clear();
+        httpClient.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+        httpClient.DefaultRequestHeaders.Add("Page-Size", "10");
+        httpClient.DefaultRequestHeaders.Add("Count", "10");
     }
+
+
+    // public static bool StartsWithUpper(this string? str)
+    // {
+    //     if (string.IsNullOrWhiteSpace(str))
+    //         return false;
+
+    //     char ch = str[0];
+    //     return char.IsUpper(ch);
+    // }
 }
--- MtgClientTest/UnitTest1.cs
+++ MtgClientTest/UnitTest1.cs
@@ -16,45 +16,52 @@ public class Tests
     }
 
     [Test]
-    public void TestStartsWithUpper()
+    public void TestClient()
     {
-        // Tests that we expect to return true.
-        string[] words = { "Alphabet", "Zebra", "ABC", "Αθήνα", "Москва" };
-        foreach (var word in words)
-        {
-            bool result = word.StartsWithUpper();
-            Assert.IsTrue(result,
-                   string.Format("Expected for '{0}': true; Actual: {1}",
-                                 word, result));
-        }
+        var client = new Client();
+        Assert.IsTrue(client.httpClient.GetType() == typeof(System.Net.Http.HttpClient));
     }
 
-    [Test]
-    public void TestDoesNotStartWithUpper()
-    {
-        // Tests that we expect to return false.
-        string[] words = { "alphabet", "zebra", "abc", "αυτοκινητοβιομηχανία", "государство",
-                               "1234", ".", ";", " " };
-        foreach (var word in words)
-        {
-            bool result = word.StartsWithUpper();
-            Assert.IsFalse(result,
-                   string.Format("Expected for '{0}': false; Actual: {1}",
-                                 word, result));
-        }
-    }
+    // [Test]
+    // public void TestStartsWithUpper()
+    // {
+    //     // Tests that we expect to return true.
+    //     string[] words = { "Alphabet", "Zebra", "ABC", "Αθήνα", "Москва" };
+    //     foreach (var word in words)
+    //     {
+    //         bool result = word.StartsWithUpper();
+    //         Assert.IsTrue(result,
+    //                string.Format("Expected for '{0}': true; Actual: {1}",
+    //                              word, result));
+    //     }
+    // }
 
-    [Test]
-    public void DirectCallWithNullOrEmpty()
-    {
-        // Tests that we expect to return false.
-        string?[] words = { string.Empty, null };
-        foreach (var word in words)
-        {
-            bool result = word.StartsWithUpper();
-            Assert.IsFalse(result,
-                   string.Format("Expected for '{0}': false; Actual: {1}",
-                                 word == null ? "<null>" : word, result));
-        }
-    }
+    // [Test]
+    // public void TestDoesNotStartWithUpper()
+    // {
+    //     // Tests that we expect to return false.
+    //     string[] words = { "alphabet", "zebra", "abc", "αυτοκινητοβιομηχανία", "государство",
+    //                            "1234", ".", ";", " " };
+    //     foreach (var word in words)
+    //     {
+    //         bool result = word.StartsWithUpper();
+    //         Assert.IsFalse(result,
+    //                string.Format("Expected for '{0}': false; Actual: {1}",
+    //                              word, result));
+    //     }
+    // }
+
+    // [Test]
+    // public void DirectCallWithNullOrEmpty()
+    // {
+    //     // Tests that we expect to return false.
+    //     string?[] words = { string.Empty, null };
+    //     foreach (var word in words)
+    //     {
+    //         bool result = word.StartsWithUpper();
+    //         Assert.IsFalse(result,
+    //                string.Format("Expected for '{0}': false; Actual: {1}",
+    //                              word == null ? "<null>" : word, result));
+    //     }
+    // }
 }
